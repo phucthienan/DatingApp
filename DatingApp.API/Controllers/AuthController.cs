b338b9cea2e32e4945f05541787d24caef6de105
Nguyen Phuc Thien An <npta97@gmail.com>
2018-10-06 20:13:55 +0700

    Add JWT Authentication

---
+++ DatingApp.API/Controllers/AuthController.cs
@@ -0,0 +1,84 @@
+using System;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+using System.Threading.Tasks;
+using DatingApp.API.Data;
+using DatingApp.API.Dtos;
+using DatingApp.API.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.IdentityModel.Tokens;
+
+namespace DatingApp.API.Controllers
+{
+    [Route("/api/[controller]")]
+    [ApiController]
+    public class AuthController : ControllerBase
+    {
+        private readonly IConfiguration _configuration;
+        private readonly IAuthRepository _authRepository;
+
+        public AuthController(IConfiguration configuration, IAuthRepository authRepository)
+        {
+            _configuration = configuration;
+            _authRepository = authRepository;
+        }
+
+        [HttpPost("register")]
+        public async Task<IActionResult> Register(UserForLoginDto userForRegisterDto)
+        {
+            userForRegisterDto.Username = userForRegisterDto.Username.ToLower();
+
+            if (await _authRepository.UserExists(userForRegisterDto.Username))
+            {
+                return BadRequest("Username already exists.");
+            }
+
+            var userToCreate = new User
+            {
+                Username = userForRegisterDto.Username
+            };
+
+            var createdUser = await _authRepository.Register(userToCreate, userForRegisterDto.Password);
+            // return CreatedAtRoute();
+            return StatusCode(201);
+        }
+
+        [HttpPost("login")]
+        public async Task<IActionResult> Login(UserForLoginDto userForLoginDto)
+        {
+            var user = await _authRepository.Login(
+                userForLoginDto.Username.ToLower(),
+                userForLoginDto.Password);
+
+            if (user == null)
+            {
+                return Unauthorized();
+            }
+
+            var claims = new[]
+            {
+                new Claim(ClaimTypes.NameIdentifier, user.Id.ToString()),
+                new Claim(ClaimTypes.Name, user.Username)
+            };
+
+            var key = new SymmetricSecurityKey(
+                Encoding.UTF8.GetBytes(_configuration.GetSection("AppSettings:Token").Value));
+
+            var credentials = new SigningCredentials(key, SecurityAlgorithms.HmacSha512Signature);
+
+            var tokenDescriptor = new SecurityTokenDescriptor
+            {
+                Subject = new ClaimsIdentity(claims),
+                Expires = DateTime.Now.AddDays(1),
+                SigningCredentials = credentials
+            };
+
+            var tokenHandler = new JwtSecurityTokenHandler();
+            var token = tokenHandler.CreateToken(tokenDescriptor);
+
+            return Ok(new { token = tokenHandler.WriteToken(token) });
+        }
+    }
+}
